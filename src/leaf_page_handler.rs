637871d84dfb8329b9c6fb395d409644fcdbc137
Mark Mc Keown <mark.mckeown@cirata.com>
2025-10-04 23:48:32 +0000

    leaf_page_handler

--- src/leaf_page_handler.rs
+++ src/leaf_page_handler.rs
@@ -1,13 +1,44 @@
+use crate::page::PageTrait;
 use crate::tree_leaf_page::TreeLeafPage;
 use crate::tuple::{Tuple, TupleTrait};
+use crate::free_page_tracker::FreePageTracker;
+use crate::page_cache::PageCache;
 
 pub struct LeafPageHandler {
-   
 }
 
 impl LeafPageHandler {
     // This happens after overflow handling, so we know the tuple will fit in a page.
-    pub fn add_to_page(tuple: Tuple, 
+    pub fn add_tuple(page: TreeLeafPage, 
+                    tuple: Tuple,
+                    free_page_tracker: &mut FreePageTracker,
+                    page_cache: &mut PageCache,
+                    version: u64, 
+                    page_size: usize) -> Vec<TreeLeafPage> {
+        let mut pages: Vec<TreeLeafPage> = Vec::new();
+        pages.push(page);
+        LeafPageHandler::add_to_page(tuple, &mut pages, page_size);
+        LeafPageHandler::map_pages(&mut pages, free_page_tracker, page_cache, version);
+        pages
+    }
+
+    fn map_pages(pages: &mut Vec<TreeLeafPage>, 
+                    free_page_tracker: &mut FreePageTracker, 
+                    page_cache: &mut PageCache, 
+                    version: u64) -> () {
+        for page in pages {
+            let old_page_no = page.get_page_number();
+            if old_page_no != 0 {
+                free_page_tracker.return_free_page_no(old_page_no);
+            }
+            let new_page_no = free_page_tracker.get_free_page(page_cache);
+            page.set_page_number(new_page_no);
+            page.set_version(version);
+        }
+    }
+
+    
+    fn add_to_page(tuple: Tuple, 
                 new_pages: &mut Vec<TreeLeafPage>,
                 page_size: usize) -> () {
         assert!(!new_pages.is_empty());
