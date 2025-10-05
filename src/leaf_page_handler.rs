b7f25c7582563be2a7cf918e251503ec8a638bdf
Mark Mc Keown <mark.mckeown@cirata.com>
2025-10-05 12:50:25 +0000

    Added more testing

--- src/leaf_page_handler.rs
+++ src/leaf_page_handler.rs
@@ -105,7 +105,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add_remove_pages() {
+    fn test_small_tuple_split_page() {
         let temp_file = tempfile::NamedTempFile::new().expect("Failed to create temp file");
         let db_file = std::fs::OpenOptions::new()
             .write(true)
@@ -131,14 +131,18 @@ mod tests {
         
         let mut new_version = version;
         let mut tree_leaf_page: TreeLeafPage;
+        let mut tuple: Tuple;
+        let mut j: u32 = 0;
         // Each loop is a new commit.
         for i in 1u32..1024 {
+            j = i;
             new_version = new_version + 1;
-            let tuple = Tuple::new(i.to_le_bytes().to_vec(), i.to_le_bytes().to_vec(), new_version);
+            tuple = Tuple::new(i.to_le_bytes().to_vec(), i.to_le_bytes().to_vec(), new_version);
             tree_leaf_page = TreeLeafPage::from_page(page_cache.get_page(tree_leaf_page_no));
             let mut free_page_tracker = FreePageTracker::new( 
                  page_cache.get_page(free_dir_page_no), new_version, crate::Db::PAGE_SIZE as usize);
 
+            // Fill page, but don't split.
             if !tree_leaf_page.can_fit(tuple.get_byte_size()) {
                 break;
             }     
@@ -155,11 +159,93 @@ mod tests {
             page_cache.put_page(free_dir_page.get_page());
         }
 
-        // We should be using 18 pages - the two created at the start and the free_pages_allocated.
+        // We should be using 18 pages - the two created at the start and the free_pages allocated
+        // by the free page allocator
         assert_eq!(page_cache.get_total_page_count(), 18);
         // There should be 16 free pages of the 18, 2 used to store page_dir and the tree_leaf
         assert_eq!(free_dir_page.get_entries(), 16);
 
+
+        // Original page is full now - next input will require a split
+        tuple = Tuple::new(j.to_le_bytes().to_vec(), j.to_le_bytes().to_vec(), new_version);
+        let mut free_page_tracker = FreePageTracker::new( 
+                 page_cache.get_page(free_dir_page_no), new_version, crate::Db::PAGE_SIZE as usize);
+        tree_leaf_page = TreeLeafPage::from_page(page_cache.get_page(tree_leaf_page_no));
+        let mut pages = LeafPageHandler::add_tuple(tree_leaf_page, tuple, &mut free_page_tracker,
+                &mut page_cache, new_version, crate::Db::PAGE_SIZE as usize);
+        assert!(pages.len() == 2);
+        let tree_leaf_page1 = pages.pop().unwrap();
+        assert_eq!(tree_leaf_page1.get_all_tuples(crate::Db::PAGE_SIZE as usize).len(), 93);
+        let tree_leaf_page2 = pages.pop().unwrap();
+        assert_eq!(tree_leaf_page2.get_all_tuples(crate::Db::PAGE_SIZE as usize).len(), 93);
+            
+        let mut free_dir_pages = free_page_tracker.get_free_dir_pages(&mut page_cache);  
+        assert!(free_dir_pages.len() == 1);  
+        free_dir_page = free_dir_pages.pop().unwrap();
+
+        // Used an extra free page.
+        assert_eq!(free_dir_page.get_entries(), 15);
+
         std::fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
     }
+
+     #[test]
+    fn test_add_big_pages() {
+        let temp_file = tempfile::NamedTempFile::new().expect("Failed to create temp file");
+        let db_file = std::fs::OpenOptions::new()
+            .write(true)
+            .read(true)
+            .create(true)
+            .open(&temp_file).expect("Failed to open or create DB file");
+
+        let version = 0;
+        let file_layer: crate::FileLayer = crate::FileLayer::new(db_file, crate::Db::PAGE_SIZE);
+        let block_layer: crate::BlockLayer = crate::BlockLayer::new(file_layer, crate::Db::PAGE_SIZE);
+        let mut page_cache: PageCache = PageCache::new(block_layer, crate::Db::PAGE_SIZE);
+
+        let mut free_dir_page_no = *page_cache.create_new_pages(1).get(0).unwrap();
+        let mut tree_leaf_page_no = *page_cache.create_new_pages(1).get(0).unwrap();
+        
+        let mut free_dir_page = FreeDirPage::new(crate::Db::PAGE_SIZE, free_dir_page_no, version);
+        page_cache.put_page(free_dir_page.get_page());
+
+        let mut first_tree_leaf_page: TreeLeafPage = TreeLeafPage::new(crate::Db::PAGE_SIZE, tree_leaf_page_no);
+        first_tree_leaf_page.set_version(version+1);
+        page_cache.put_page(first_tree_leaf_page.get_page());
+
+        
+        let mut new_version = version;
+        let mut tree_leaf_page: TreeLeafPage;
+        let mut tuple: Tuple;
+        
+
+        // Each loop is a new commit.
+        let mut tree_leaf_page_count = 0;
+        for i in 0u32..2 {
+            new_version = new_version + 1;
+            let value = vec![0u8; 2048];
+            tuple = Tuple::new(i.to_le_bytes().to_vec(), value, new_version);
+            tree_leaf_page = TreeLeafPage::from_page(page_cache.get_page(tree_leaf_page_no));
+            let mut free_page_tracker = FreePageTracker::new( 
+                 page_cache.get_page(free_dir_page_no), new_version, crate::Db::PAGE_SIZE as usize);
+
+            let mut pages = LeafPageHandler::add_tuple(tree_leaf_page, tuple, &mut free_page_tracker,
+                &mut page_cache, new_version, crate::Db::PAGE_SIZE as usize);
+            tree_leaf_page_count = 0;    
+            while let Some(mut tree_leaf_page) = pages.pop() {
+                tree_leaf_page_count = tree_leaf_page_count + 1;
+                tree_leaf_page_no = tree_leaf_page.get_page_number();
+                page_cache.put_page(tree_leaf_page.get_page());
+            }
+            let mut free_dir_pages = free_page_tracker.get_free_dir_pages(&mut page_cache);  
+            assert!(free_dir_pages.len() == 1);  
+            free_dir_page = free_dir_pages.pop().unwrap();
+            free_dir_page_no = free_dir_page.get_page_number();
+            page_cache.put_page(free_dir_page.get_page());
+        }
+        assert_eq!(tree_leaf_page_count, 2);
+
+        std::fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
+    }
+
 }
