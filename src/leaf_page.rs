26e2b203dbd670c080d5d81bd96ba5a8aab21456
Mark Mc Keown <mark.mckeown@cirata.com>
2026-03-23 08:33:49 +0000

    Adding tests to leaf_page

--- src/leaf_page.rs
+++ src/leaf_page.rs
@@ -117,31 +117,43 @@ impl LeafPage {
         self.page.get_page_bytes_mut()[slot_offset + 3..slot_offset + 5].copy_from_slice(&val_len_bytes);
     }
 
-    pub fn get_value_at_slot(&self, slot: Slot) -> &[u8] {
+    pub fn get_value_at_slot(&self, slot: &Slot) -> &[u8] {
         let val_offset = (slot.offset + slot.key_len as u16) as usize;
         &self.page.get_page_bytes()[val_offset..val_offset + slot.val_len as usize]
     }
 
-    pub fn get_key_at_slot(&self, slot: Slot) -> &[u8] {
+    pub fn get_key_at_slot(&self, slot: &Slot) -> &[u8] {
         let key_offset = slot.offset as usize;
         &self.page.get_page_bytes()[key_offset..key_offset + slot.key_len as usize]
     }
 
     
-    pub fn insert_entry_value(&mut self, key: &[u8], value: &[u8]) -> Slot {
-        let entry_size = key.len() + value.len() + LeafPage::SLOT_SIZE;
+    pub fn add_key_value_at_index(&mut self, index: usize, key: &[u8], value: &[u8]) {
+        // Sanity check
+        let new_entry_size = key.len() + value.len();
+        let new_entry_total_size = new_entry_size + LeafPage::SLOT_SIZE;
         let free_space = self.get_free_space() as usize;
-        assert!(entry_size <= free_space);
+        assert!(new_entry_total_size <= free_space);
 
+        // Find were the key/value entry can be added.
         let entries = self.get_entries() as usize;
-        let new_slot_offset = LeafPage::HEADER_SIZE + entries * LeafPage::SLOT_SIZE;
-        let old_entry_offset = self.page.page_size - (new_slot_offset + free_space);
-        let new_entry_offset = old_entry_offset - (key.len() as usize + value.len() as usize);
+        let header_plus_slots_size = LeafPage::HEADER_SIZE + entries * LeafPage::SLOT_SIZE;
+        let entries_size = self.page.page_size - (header_plus_slots_size + free_space);
+        let old_entries_offset = self.page.page_size - entries_size;
+        let new_entry_offset = old_entries_offset - new_entry_size;
+        
+        // Add key/value of offset
         self.page.get_page_bytes_mut()[new_entry_offset..new_entry_offset + key.len()].copy_from_slice(key);
         self.page.get_page_bytes_mut()[new_entry_offset + key.len()..new_entry_offset + key.len() + value.len()].copy_from_slice(value);
+        
+        // Create a slot and add it.
+        let slot = Slot { offset: new_entry_offset as u16, key_len: key.len() as u8, val_len: value.len() as u16 };
+        self.shift_slots_right_from(index);
+        self.set_slot_at_index(index, slot);
+
+        // Update entries and free space.
         self.set_entries((entries + 1) as u16);
-        self.set_free_space(free_space as u16 - entry_size as u16);
-        Slot { offset: new_entry_offset as u16, key_len: key.len() as u8, val_len: value.len() as u16 }
+        self.set_free_space(free_space as u16 - new_entry_total_size as u16);
     }
 
 
@@ -161,10 +173,45 @@ impl LeafPage {
         );
     }
 
-    pub fn add_key_value_at_index(&mut self, index: usize, key: &[u8], value: &[u8]) {
-        let new_slot = self.insert_entry_value(key, value);
-        self.shift_slots_right_from(index);
-        self.set_slot_at_index(index, new_slot);
-    }
     
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_data_page() {
+        let page_config = PageConfig { block_size: 4096, page_size: 4000 };
+        let mut leaf_page = LeafPage::create_new(&page_config, 1);
+        assert_eq!(leaf_page.get_entries(), 0);
+        assert_eq!(leaf_page.get_free_space(), 4000 - LeafPage::HEADER_SIZE as u16);
+
+        let key1 = b"key1";
+        let value1 = b"value1";
+        leaf_page.add_key_value_at_index(0, key1, value1);
+        assert_eq!(leaf_page.get_entries(), 1);
+        assert_eq!(leaf_page.get_free_space(), 4000 - LeafPage::HEADER_SIZE as u16 - (key1.len() + value1.len() + LeafPage::SLOT_SIZE) as u16);
+        let slot0 = leaf_page.get_slot_at_index(0);
+        assert_eq!(leaf_page.get_key_at_slot(&slot0), key1);
+        assert_eq!(leaf_page.get_value_at_slot(&slot0), value1);
+
+
+        let key2 = b"key2";
+        let value2 = b"value2";
+        leaf_page.add_key_value_at_index(0, key2, value2);
+        assert_eq!(leaf_page.get_entries(), 2);
+        let size_used = (key1.len() + value1.len() + LeafPage::SLOT_SIZE) 
+                                + (key2.len() + value2.len() + LeafPage::SLOT_SIZE);
+        assert_eq!(leaf_page.get_free_space(), 4000 - (LeafPage::HEADER_SIZE as u16 + size_used as u16));
+        let slot0 = leaf_page.get_slot_at_index(0);
+        assert_eq!(leaf_page.get_key_at_slot(&slot0), key2);
+        assert_eq!(leaf_page.get_value_at_slot(&slot0), value2);
+
+        let slot1 = leaf_page.get_slot_at_index(1);
+        assert_eq!(leaf_page.get_key_at_slot(&slot1), key1);
+        assert_eq!(leaf_page.get_value_at_slot(&slot1), value1);
+    }
+
+
 }
