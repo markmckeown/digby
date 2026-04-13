0c09f203f154815c23b8445b9891acf79dcd8876
Mark Mc Keown <mark.mckeown@cirata.com>
2026-04-13 07:55:54 +0000

    Use enum for page size.

--- src/page_no.rs
+++ src/page_no.rs
@@ -1,6 +1,6 @@
 pub struct PageNo(u64);
 
-pub enum PageSize {
+pub enum BlockSize {
     PageSize4K = 0,
     PageSize8K = 1,
     PageSize16K = 2,
@@ -12,37 +12,37 @@ pub enum PageSize {
     PageSize1M = 8,
 }
 
-impl TryFrom<u8> for PageSize {
+impl TryFrom<u8> for BlockSize {
     type Error = ();
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(PageSize::PageSize4K),
-            1 => Ok(PageSize::PageSize8K),
-            2 => Ok(PageSize::PageSize16K),
-            3 => Ok(PageSize::PageSize32K),
-            4 => Ok(PageSize::PageSize64K),
-            5 => Ok(PageSize::PageSize128K),
-            6 => Ok(PageSize::PageSize256K),
-            7 => Ok(PageSize::PageSize512K),
-            8 => Ok(PageSize::PageSize1M),
+            0 => Ok(BlockSize::PageSize4K),
+            1 => Ok(BlockSize::PageSize8K),
+            2 => Ok(BlockSize::PageSize16K),
+            3 => Ok(BlockSize::PageSize32K),
+            4 => Ok(BlockSize::PageSize64K),
+            5 => Ok(BlockSize::PageSize128K),
+            6 => Ok(BlockSize::PageSize256K),
+            7 => Ok(BlockSize::PageSize512K),
+            8 => Ok(BlockSize::PageSize1M),
             _ => Err(()),
         }
     }
 }
 
-impl PageSize {
+impl BlockSize {
     pub fn get_byte_size(&self) -> usize {
         match self {
-            PageSize::PageSize4K => 4096,
-            PageSize::PageSize8K => 8192,
-            PageSize::PageSize16K => 16384,
-            PageSize::PageSize32K => 32768,
-            PageSize::PageSize64K => 65536,
-            PageSize::PageSize128K => 131072,
-            PageSize::PageSize256K => 262144,
-            PageSize::PageSize512K => 524288,
-            PageSize::PageSize1M => 1048576,
+            BlockSize::PageSize4K => 4096,
+            BlockSize::PageSize8K => 8192,
+            BlockSize::PageSize16K => 16384,
+            BlockSize::PageSize32K => 32768,
+            BlockSize::PageSize64K => 65536,
+            BlockSize::PageSize128K => 131072,
+            BlockSize::PageSize256K => 262144,
+            BlockSize::PageSize512K => 524288,
+            BlockSize::PageSize1M => 1048576,
         }
     }
         
@@ -53,7 +53,7 @@ impl PageNo {
     const TOP_BYTE_MASK: u64 = 0xFF00_0000_0000_0000;
     const BOTTOM_56_MASK: u64 = 0x00FF_FFFF_FFFF_FFFF;
 
-    pub fn new(size: PageSize, offset: u64) -> Self {
+    pub fn new(size: BlockSize, offset: u64) -> Self {
         Self((u64::from(size as u8) << 56)| (offset & Self::BOTTOM_56_MASK))
     }
 
@@ -73,8 +73,8 @@ impl PageNo {
         self.0.to_le_bytes()
     }
 
-    pub fn get_size(&self) -> PageSize {
-        PageSize::try_from((self.0 >> 56) as u8).unwrap()
+    pub fn get_size(&self) -> BlockSize {
+        BlockSize::try_from((self.0 >> 56) as u8).unwrap()
     }
 
     pub fn set_offset(&mut self, offset: u64) {
@@ -101,7 +101,7 @@ mod tests {
         assert_eq!(page_no.get_offset(), 34);
         assert_eq!(page_no.get_bytes(), [34, 0, 0, 0, 0, 0, 0, 0]);
 
-        let mut jumbo_page = PageNo::new(PageSize::PageSize1M, 45);
+        let mut jumbo_page = PageNo::new(BlockSize::PageSize1M, 45);
         assert_eq!(jumbo_page.get_size().get_byte_size(), 1048576);
         assert_eq!(jumbo_page.get_offset(), 45);
 
