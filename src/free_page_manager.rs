0ac5d81874cc2df3a8450c39a319d372a85b49c4
Mark Mc Keown <mark.mckeown@cirata.com>
2026-07-26 17:03:43 +0100

    Implement flush_free_page_trackers in free_page_manager

--- src/free_page_manager.rs
+++ src/free_page_manager.rs
@@ -1,4 +1,5 @@
-use crate::{FreePageTracker, PageCache, PageNo};
+use crate::page::PageTrait;
+use crate::{DbMasterPage, FreePageTracker, PageCache, PageNo};
 use std::collections::HashMap;
 
 pub struct FreePageManager {
@@ -6,6 +7,7 @@ pub struct FreePageManager {
     og_free_pg_dir_pg_nos: Vec<PageNo>,
     version: u64,
     db_config: crate::db_config::DbConfig,
+    flushed: bool,
 }
 
 impl FreePageManager {
@@ -19,18 +21,48 @@ impl FreePageManager {
             og_free_pg_dir_pg_nos,
             version,
             db_config,
+            flushed: false,
         }
     }
 
     pub fn get_free_page(&mut self, page_cache: &mut PageCache, blk_exp: u8) -> PageNo {
+        assert!(
+            !self.flushed,
+            "Cannot get free page after flushing free page trackers."
+        );
         self.get_free_page_tracker(page_cache, blk_exp)
             .get_free_page(page_cache)
     }
 
     pub fn return_free_page_no(&mut self, page_cache: &mut PageCache, page_no: PageNo) {
+        assert!(
+            !self.flushed,
+            "Cannot return free page after flushing free page trackers."
+        );
         let blk_exp = page_no.get_blk_cnt_exp();
         self.get_free_page_tracker(page_cache, blk_exp)
             .return_free_page_no(page_no);
+    }
+
+    pub fn flush_free_page_trackers(
+        &mut self,
+        master_page: &mut DbMasterPage,
+        page_cache: &mut PageCache,
+    ) {
+        assert!(
+            !self.flushed,
+            "Cannot flush free page trackers after they have already been flushed."
+        );
+        self.flushed = true;
+        for (blk_exp, tracker) in &mut self.free_pg_trackers {
+            let mut free_dir_pages = tracker.get_free_dir_pages(page_cache);
+            assert!(!free_dir_pages.is_empty());
+            let first_free_dir_page = free_dir_pages.last().unwrap().get_page_number();
+            while let Some(mut free_dir_page) = free_dir_pages.pop() {
+                page_cache.put_page(free_dir_page.get_page());
+            }
+            master_page.set_free_page_dir_page_no(*blk_exp, first_free_dir_page);
+        }
     }
 
     fn get_free_page_tracker(
