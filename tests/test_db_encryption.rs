ba3f307e13a976a4c5a7948af58d989a9eb6a72c
Mark Mc Keown <mark.mckeown@cirata.com>
2026-08-15 13:05:36 +0100

    Use Db::create/open in encryption tests.

--- tests/test_db_encryption.rs
+++ tests/test_db_encryption.rs
@@ -1,126 +1,110 @@
+use digby::BlockSanity;
 use digby::Db;
-use digby::compressor::CompressorType;
-use std::fs;
-use tempfile::NamedTempFile;
+use digby::db_config::DbConfig;
+use tempfile::TempDir;
 
 #[test]
 fn test_db_store_value_with_encryption() {
-    let temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    let dir = TempDir::new().expect("Failed to create temp dir");
+    let file_path = dir.path().join("db");
+    let db_path = file_path.to_str().unwrap();
+
     let enc_key = b"the_encryption_key".to_vec();
     let key = b"the_key".to_vec();
     let value = b"the_value".to_vec();
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::None,
-        );
+        let db_config = DbConfig::builder()
+            .block_sanity(BlockSanity::Aes128Gcm)
+            .build();
+        let mut db = Db::create(db_path, Some(enc_key.to_vec()), &db_config).unwrap();
         db.put(key.as_ref(), value.as_ref());
     }
     // The new scope essentially closes the DB - when Files run out of scope then
     // they are close, Rust bizairely does not allow error handling on close!
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::None,
-        );
+        let mut db = Db::open(db_path, Some(enc_key.to_vec())).unwrap();
         let returned_value = db.get(key.as_ref()).unwrap();
         assert!(returned_value == value);
     }
-    fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
 }
 
 #[test]
 #[should_panic(expected = "Failed to decrypt page")]
 fn test_db_store_value_with_encryption_wrong_key() {
-    let temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    let dir = TempDir::new().expect("Failed to create temp dir");
+    let file_path = dir.path().join("db");
+    let db_path = file_path.to_str().unwrap();
+
     let enc_key = b"the_encryption_key".to_vec();
     let key = b"the_key".to_vec();
     let value = b"the_value".to_vec();
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::None,
-        );
+        let db_config = DbConfig::builder()
+            .block_sanity(BlockSanity::Aes128Gcm)
+            .build();
+        let mut db = Db::create(db_path, Some(enc_key.to_vec()), &db_config).unwrap();
         db.put(key.as_ref(), value.as_ref());
     }
     // The new scope essentially closes the DB - when Files run out of scope then
     // they are close, Rust bizairely does not allow error handling on close!
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(b"bad_encryption_key".to_vec()),
-            CompressorType::None,
-        );
+        let mut db = Db::open(db_path, Some(b"bad_encryption_key".to_vec())).unwrap();
         let returned_value = db.get(key.as_ref()).unwrap();
         assert!(returned_value == value);
     }
-    fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
 }
 
 #[test]
 #[should_panic(expected = "Calculated checksum does not match stored checksum for page")]
 fn test_db_store_value_with_encryption_no_key() {
-    let temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    let dir = TempDir::new().expect("Failed to create temp dir");
+    let file_path = dir.path().join("db");
+    let db_path = file_path.to_str().unwrap();
+
     let enc_key = b"the_encryption_key".to_vec();
     let key = b"the_key".to_vec();
     let value = b"the_value".to_vec();
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::None,
-        );
+        let db_config = DbConfig::builder()
+            .block_sanity(BlockSanity::Aes128Gcm)
+            .build();
+        let mut db = Db::create(db_path, Some(enc_key.to_vec()), &db_config).unwrap();
         db.put(key.as_ref(), value.as_ref());
     }
     // The new scope essentially closes the DB - when Files run out of scope then
     // they are close, Rust bizairely does not allow error handling on close!
     {
-        let _db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            None,
-            CompressorType::None,
-        );
+        Db::open(db_path, None).unwrap();
     }
-    fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
 }
 
 #[test]
 fn test_db_store_large_key_value_compressible_encryption() {
-    let temp_file = NamedTempFile::new().expect("Failed to create temp file");
+    let dir = TempDir::new().expect("Failed to create temp dir");
+    let file_path = dir.path().join("db");
+    let db_path = file_path.to_str().unwrap();
+
     let key: Vec<u8> = vec![111u8; 8192];
     let value: Vec<u8> = vec![56u8; 18192];
     let enc_key = b"the_encryption_key".to_vec();
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::LZ4,
-        );
+        let db_config = DbConfig::builder()
+            .block_sanity(BlockSanity::Aes128Gcm)
+            .build();
+        let mut db = Db::create(db_path, Some(enc_key.to_vec()), &db_config).unwrap();
         db.put(key.as_ref(), value.as_ref());
     }
     // The new scope essentially closes the DB - when Files run out of scope then
     // they are close, Rust bizairely does not allow error handling on close!
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::LZ4,
-        );
+        let mut db = Db::open(db_path, Some(enc_key.to_vec())).unwrap();
         let returned_value = db.get(key.as_ref()).unwrap();
         assert!(returned_value == value);
         assert!(db.delete(&key));
     }
     {
-        let mut db = Db::new(
-            temp_file.path().to_str().unwrap(),
-            Some(enc_key.to_vec()),
-            CompressorType::LZ4,
-        );
+        let mut db = Db::open(db_path, Some(enc_key.to_vec())).unwrap();
         let returned_value = db.get(key.as_ref());
         assert!(returned_value.is_none());
     }
-    fs::remove_file(temp_file.path()).expect("Failed to remove temp file");
 }
